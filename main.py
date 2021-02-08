60e305d5b6c7b5770f9f54e31ee9ffd8612afc79
changhoetyng <hoetyng@gmail.com>
2021-02-08 23:53:54 +0800

    second meeting

--- main.py
+++ main.py
@@ -27,11 +27,18 @@
 output = []
 
 
+def checkServedPizza(checkMostIngredient,servedPizza,mostIngredient,mylist):
+  for served in servedPizza:
+    if checkMostIngredient == served:
+      return False
+    if mostIngredient == int(mylist[checkMostIngredient].split(" ")[0]):
+      return False
 
+  return True
 
 
 def loop():
-    with open('C:/Users/Jason/Desktop/Hashcode/b_little_bit_of_everything.in') as f:
+    with open('b_little_bit_of_everything.in') as f:
       mylist = [line.rstrip('\n') for line in f]
 
     firstLine = mylist[0].split(" ")
@@ -41,19 +48,52 @@ def loop():
     p3 = int(firstLine[2])
     p4 = int(firstLine[3])
 
-    lineCounter = 1
+    # lineCounter = 1
     fixCounter = 1
     # totalPizza+=1
     swap = False
     maxIngredient = 0
 
+
+
+    #pizza that are already served (BY ROW)
+    servedPizza=[]
+
+    #Store ouput
     maxIngredientLine=[]
-    for x in range(4):
+    # LOOP 1.0 
+    for x in range(2):
+
+        # Line with most ingredient
+        mostIngredientLine = -1
+
+        # Number of most ingredient
+        mostIngredient = -1
+
+        lineCounter = 1
+        checkMostIngredient = 1
+
+        
+        # LOOP CHECK IF PIZZA IS SERVED
         for x in range(totalPizza):
-            if x==0:
-                continue
-            fixLine = mylist[fixCounter].split(" ")
-            compareLine = mylist[fixCounter + lineCounter].split(" ")
+          breakBool = False
+          for served in servedPizza:
+              if x == served:
+                breakBool = True
+
+          if x==0 or breakBool == True:
+              continue
+        
+        # FIND MOST INGREDIENT IN FILE
+        # LAST 2 VAR
+          if mostIngredient < int(mylist[checkMostIngredient].split(" ")[0]) and checkServedPizza(checkMostIngredient,servedPizza,mostIngredient,mylist):
+              mostIngredientLine = checkMostIngredient
+              mostIngredient = int(mylist[checkMostIngredient].split(" ")[0])
+          checkMostIngredient+=1
+        # LOOP 1.1
+        for x in range(totalPizza):
+            fixLine = mylist[mostIngredientLine].split(" ")
+            compareLine = mylist[lineCounter].split(" ")
             #swapLine
             if int(fixLine[0]) < int(compareLine[0]):
                 temp = fixLine
@@ -72,24 +112,27 @@ def loop():
                         continue
                     if fixLine[i]==compareLine[j]:
                         counter-=1
-
+            
             if counter > maxIngredient:
                 temp = counter
                 counter = maxIngredient
                 maxIngredient = temp
                 maxLine = fixCounter + lineCounter
 
-
+          # SWAP LARGER LINES WITH SMALLER LINES
             if swap == True:
                 temp = fixLine
                 fixLine = compareLine
                 compareLine = temp
                 swap = False
-
             lineCounter+=1
+      
         maxIngredientLine.append(4)
-        maxIngredientLine.append(fixCounter)
+        maxIngredientLine.append(mostIngredientLine)
         maxIngredientLine.append(maxLine)
+        servedPizza.append(mostIngredientLine)
+        servedPizza.append(maxLine)
+        print(servedPizza)
         p4-=1
         print(maxIngredientLine)
         print(p4)
